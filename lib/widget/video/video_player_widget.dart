274fc9aeeb0b5f6dc997d976bc73c53788673736
Mạnh Lê <levanmanh24062002@gmail.com>
2025-11-03 22:42:14 +0700

    fix thumbnail video

--- lib/widget/video/video_player_widget.dart
+++ lib/widget/video/video_player_widget.dart
@@ -1,82 +1,118 @@
 import 'dart:io';
-
-import 'package:chats/main.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:chats/main.dart';
 
 class VideoPlayerWidget extends StatefulWidget {
-  const VideoPlayerWidget({super.key, required this.videoPath, this.isLocal = false});
   final String videoPath;
   final bool isLocal;
 
+  const VideoPlayerWidget({
+    super.key,
+    required this.videoPath,
+    this.isLocal = false,
+  });
+
   @override
-  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
+  State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
 }
 
-class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
-  late VideoPlayerController _controller;
-  final ValueNotifier<bool> _isPlayingNotifier = ValueNotifier(false);
-  final ValueNotifier<bool> _isInitializedNotifier = ValueNotifier(false);
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget>
+    with AutomaticKeepAliveClientMixin {
+  VideoPlayerController? _controller;
+  bool _isInitialized = false;
+  String? _currentPath;
+
+  @override
+  bool get wantKeepAlive => true;
 
   @override
   void initState() {
     super.initState();
-    if (kIsWeb) {
-      _controller = widget.isLocal
-          ? VideoPlayerController.networkUrl(Uri.parse(widget.videoPath))
-          : VideoPlayerController.networkUrl(Uri.parse(widget.videoPath))
-        ..initialize().then((_) {
-          _isInitializedNotifier.value = true;
-          _isPlayingNotifier.value = _controller.value.isPlaying;
-        });
-    } else {
-      _controller = widget.isLocal
-          ? VideoPlayerController.file(File(widget.videoPath))
-          : VideoPlayerController.networkUrl(Uri.parse(widget.videoPath))
-        ..initialize().then((_) {
-          _isInitializedNotifier.value = true;
-          _isPlayingNotifier.value = _controller.value.isPlaying;
-        });
-    }
+    _initController(widget.videoPath);
+  }
 
-    _controller.addListener(() {
-      _isPlayingNotifier.value = _controller.value.isPlaying;
-    });
+  Future<void> _initController(String path) async {
+    if (_currentPath == path && _controller != null) return;
+
+    _currentPath = path;
+    _controller?.dispose();
+
+    final controller = kIsWeb
+        ? VideoPlayerController.networkUrl(Uri.parse(path))
+        : (widget.isLocal
+            ? VideoPlayerController.file(File(path))
+            : VideoPlayerController.networkUrl(Uri.parse(path)));
+
+    setState(() => _isInitialized = false);
+
+    try {
+      await controller.initialize();
+      setState(() {
+        _controller = controller;
+        _isInitialized = true;
+      });
+    } catch (e) {
+      debugPrint('Video init error: $e');
+    }
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ValueListenableBuilder<bool>(
-        valueListenable: _isInitializedNotifier,
-        builder: (context, isInitialized, child) {
-          if (!isInitialized) {
-            return Center(child: CircularProgressIndicator(color: appTheme.appColor));
-          }
-          return ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: appTheme.grayColor.withOpacity(0.5),
-              ),
-              child: AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              ),
-            ),
-          );
-        },
-      ),
-    );
+  void didUpdateWidget(VideoPlayerWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.videoPath != widget.videoPath) {
+      _initController(widget.videoPath);
+    }
   }
 
   @override
   void dispose() {
-    _controller.dispose();
-    _isPlayingNotifier.dispose();
-    _isInitializedNotifier.dispose();
+    _controller?.dispose();
     super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+
+    if (!_isInitialized || _controller == null) {
+      return Container(
+        alignment: Alignment.center,
+        height: 180,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: appTheme.grayColor.withOpacity(0.2),
+        ),
+        child: CircularProgressIndicator(color: appTheme.appColor, strokeWidth: 2),
+      );
+    }
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: AspectRatio(
+        aspectRatio: _controller!.value.aspectRatio,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            VideoPlayer(_controller!),
+            if (!_controller!.value.isPlaying)
+              const Icon(Icons.play_circle_fill, color: Colors.white70, size: 48),
+            Positioned.fill(
+              child: GestureDetector(
+                onTap: () {
+                  if (_controller!.value.isPlaying) {
+                    _controller!.pause();
+                  } else {
+                    _controller!.play();
+                  }
+                  setState(() {});
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
